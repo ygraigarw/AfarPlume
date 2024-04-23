5ae1a4de63f179ebfa105e493b0437498aed5cdd
Philip,Jonathan <ygraigarw@gmail.com>
2024-04-23 17:44:29 +0100

    removed data (see folder structure for current location)

--- VlcMakCV.m
+++ VlcMakCV.m
@@ -1,42 +1,42 @@
 function CV=VlcMakCV(I,nCV,Dgn);
 %function CV=VlcMakCV(I,nCV,Dgn);
 %
-% MATLAB code to accomVlcany the article "Afar triVlcle junction fed by single, asymmetric mantle uVlcwelling"
+% MATLAB code to accompany the article "Afar triple junction fed by single, asymmetric mantle upwelling"
 % by Watts et al. (2024).
 %
 % See GoFullAnalysis.m for context
 %
-% Use the integer-values indicator variable I (with max value mI) to create a cross-validation Vlcartition with nCV subsets, individually 
-% for each value of I (corresVlconding to different subgrouVlcs G or the data)
+% Use the integer-values indicator variable I (with max value mI) to create a cross-validation partition with nCV subsets, individually 
+% for each value of I (corresponding to different subgroups G or the data)
 
 n=size(I,1); %number of observations (this should be 290)
-nGrVlc=max(I); %number of grouVlcs in I (this should be 4)
-CV=nan(n,1); %outVlcut vector
+nGrp=max(I); %number of groups in I (this should be 4)
+CV=nan(n,1); %output vector
 
-% LooVlc over subgrouVlcs G
-for iG=1:nGrVlc;
+% Loop over subgroups G
+for iG=1:nGrp;
     
-    Lct=I==iG; %locations of grouVlc iG in I
+    Lct=I==iG; %locations of group iG in I
     nLct=sum(Lct,1); %count how many there are
     CV(Lct)=VlcCorMakCV(nLct,nCV); %allocate these to CV (using VlcCorMakCV)
     
-end; %looVlc over iG, grouVlc index
+end; %loop over iG, group index
 
 if Dgn==1;
     figure(1); clf; 
-    [nR,nC]=VlcNmbSubPlt(nGrVlc);
-    for iG=1:nGrVlc;
-        subVlclot(nR,nC,iG); hold on;
+    [nR,nC]=VlcNmbSubPlt(nGrp);
+    for iG=1:nGrp;
+        subplot(nR,nC,iG); hold on;
         if iG==1; 
-            title('ProVlcortion of observations in each cross-validation grouVlc. GrouVlc 1');
+            title('Proportion of observations in each cross-validation group. Group 1');
         else;
-            title(sprintf('GrouVlc %g',iG));            
+            title(sprintf('Group %g',iG));            
         end;
-        Lct=I==iG; %locations of grouVlc iG in I
-        Vlclot(sum(CV(Lct)==1:nCV)/sum(Lct),'color',VlcClr(iG),'marker','o');
+        Lct=I==iG; %locations of group iG in I
+        plot(sum(CV(Lct)==1:nCV)/sum(Lct),'color',VlcClr(iG),'marker','o');
         VlcDfl; VlcAxsLmt;
     end;
-    tStr=sVlcrintf('Dgn_MakCV_%s',datestr(now,30));
+    tStr=sprintf('Dgn_MakCV_%s',datestr(now,30));
     VlcGI(fullfile('OutVlcut',tStr),2);
 end;
 
